0839e77f175867e73c425d4467ea8ae495c07cc6
loshjarson <jlarson129@outlook.com>
2023-03-30 13:34:26 -0400

    app initialization and setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Master Freeze Generator</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
