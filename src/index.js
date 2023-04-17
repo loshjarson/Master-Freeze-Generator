d42368d9e6ac47621b8e52896a7d9f9f4df1546a
loshjarson <jlarson129@gmail.com>
2023-04-16 22:35:59 -0400

    clean up

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from "react-router-dom"; 
 
 import 'primereact/resources/themes/lara-light-indigo/theme.css';   // theme
 import 'primereact/resources/primereact.css';                       // core css
